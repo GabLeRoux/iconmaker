ca6983a7faa3ce0162ebe19ad979547d56a4f1cd
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-10-26 10:50:42 -0400

    Simplify configuration using setup.cfg

--- setup.py
+++ setup.py
@@ -3,35 +3,29 @@
 iconmaker
 =========
 
-iconmaker is a simple Python library providing an interface for easily 
-converting PNG or GIF source files to Microsoft Windows ICO or Apple Mac ICNS 
+iconmaker is a simple Python library providing an interface for easily
+converting PNG or GIF source files to Microsoft Windows ICO or Apple Mac ICNS
 icon container formats.
 """
 
 from setuptools import setup, find_packages
 
 tests_require = [
-    'nose', 
-    'mysql-connector-repackaged'
+    'nose',
 ]
 
 install_requires = [
-#    'PIL>=1.1.7', 
-    'requests>=0.14.1', 
+    'requests>=2.20.0',
 ]
 
 setup(
-    name = 'iconmaker', 
-    version = '1.0.0', 
-    url = 'http://www.iconfinder.com', 
-    description = 'Icon conversion utility', 
-    long_description = __doc__, 
-    packages = find_packages(exclude = ['tests', 
-                                        '.*', 
-                                        'venv']), 
-    install_requires = install_requires, 
-    tests_require = tests_require, 
-    extras_require = {
-        'test': tests_require
-    },
+    name='iconmaker',
+    version='1.0.0',
+    url='http://www.iconfinder.com',
+    description='Icon conversion utility',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests', '.*', 'venv']),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
 )
